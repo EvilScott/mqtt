fd0137aa8c1f5153cd88ba17ab884e9242f3ae68
EvilScott <reis.robert.s@gmail.com>
2022-09-15 17:22:56 -0700

    add data type encoding/decoding and tests

--- src/control_packet.rs
+++ src/control_packet.rs
@@ -1,7 +1,7 @@
-use crate::control_packet::connect::Connect;
 use crate::fixed_header::FixedHeader;
-use crate::payload::Payload;
 use crate::variable_header::VariableHeader;
+use crate::payload::Payload;
+use crate::control_packet::connect::Connect;
 
 pub(crate) mod connect;
 
@@ -52,8 +52,64 @@ pub(crate) fn parse_packet_bytes(bytes: Vec<u8>) -> Box<dyn ControlPacket> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::{encode_variable_length_int, decode_variable_length_int, encode_utf8_string};
+
+    #[test]
+    fn test_encode_variable_length_int() {
+        let actual: Vec<u8> = encode_variable_length_int(128);
+        let expected: Vec<u8> = vec![0x80, 0x01];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_decode_variable_length_int() {
+        let bytes: Vec<u8> = vec![0x80, 0x01, 0xFF, 0x30];
+        let actual: (u32, usize) = decode_variable_length_int(bytes);
+        let expected: (u32, usize) = (128, 2);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_encode_utf8_string() {
+        let actual: Vec<u8> = encode_utf8_string("foobar");
+        let expected: Vec<u8> = vec![0,6,102,111,111,98,97,114];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_encode_utf8_string_empty() {
+        let actual: Vec<u8> = encode_utf8_string("");
+        let expected: Vec<u8> = vec![0,0];
+        assert_eq!(actual, expected);
+    }
+}
+
+pub(crate) fn encode_variable_length_int(mut int: u32) -> Vec<u8> {
+    let mut bytes: Vec<u8> = vec![];
+    loop {
+        let mut byte: u8 = (int % 128) as u8;
+        int = int / 128;
+        if int > 0 { byte = byte | 128; }
+        bytes.push(byte);
+        if int == 0 { return bytes; }
+    }
+}
+
+pub(crate) fn decode_variable_length_int(bytes: Vec<u8>) -> (u32, usize) {
+    let mut multiplier: u32 = 1;
+    let mut value: u32 = 0;
+    for (idx, byte) in bytes.iter().enumerate() {
+        value = value + ((byte & 127) as u32 * multiplier) as u32;
+        multiplier = multiplier * 128;
+        if byte & 128 == 0 { return (value, idx+1); }
+    }
+    panic!("malformed variable length int");
+}
+
 pub(crate) fn encode_utf8_string(string: &str) -> Vec<u8> {
-    let mut bytes: Vec<u8> = Vec::from(string.len().to_be_bytes());
+    let mut bytes: Vec<u8> = Vec::from((string.len() as u16).to_be_bytes());
     bytes.append(&mut Vec::from(string.as_bytes()));
     bytes
 }
